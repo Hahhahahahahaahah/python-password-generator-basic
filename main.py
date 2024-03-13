b3caa6b1396efb57869a59d8b43e09df97dc4bf0
Hai Guan <davenkwek2010@gmail.com>
2024-03-13 17:53:52 +0800

    hi

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Write you random password generator program here!
+import random
+import string 
+pun = string.punctuation
+letters = string.ascii_letters
+numbers = string.digits
+combined = pun + letters + numbers
+def generate_password(len:int):
+    pw = ""
+    for char in range(len):
+        pw += random.choice(combined)
+    return pw
+    
+password1 = generate_password(5)
+print(password1)
